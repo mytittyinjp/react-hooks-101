60280423ea08107289a2265415083986e5d283b6
n_horie <mytittyinjp@gmail.com>
2020-09-11 17:26:32 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a template for React app</div>;
+  const [count, setCount] = useState(0);
+
+  const incrementCounthandler = () => {
+    setCount(count + 1);
+  };
+  const incrementCounthandler2 = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrementCounthandler = () => {
+    setCount(count - 1);
+  };
+
+  const decrementCounthandler2 = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const resetCounthandler = () => {
+    setCount(0);
+  };
+  const doubleCountHandler = () => {
+    setCount(count * 2);
+  };
+  const divide3Handler = () => {
+    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+  };
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={incrementCounthandler}>+</button>
+        <button onClick={decrementCounthandler}>-</button>
+      </div>
+      <div>
+        <button onClick={incrementCounthandler2}>+</button>
+        <button onClick={decrementCounthandler2}>-</button>
+      </div>
+      <div>
+        <button onClick={resetCounthandler}>Reset</button>
+        <button onClick={doubleCountHandler}>*2</button>
+        <button onClick={divide3Handler}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
