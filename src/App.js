11bed585735a9ac0f9953720636998279f133e41
n_horie <mytittyinjp@gmail.com>
2020-09-11 17:40:24 +0900

    increment component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const incrementCounthandler = () => {
-    setCount(count + 1);
-  };
-  const incrementCounthandler2 = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrementCounthandler = () => {
-    setCount(count - 1);
-  };
-
-  const decrementCounthandler2 = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
-  const resetCounthandler = () => {
-    setCount(0);
-  };
-  const doubleCountHandler = () => {
-    setCount(count * 2);
-  };
-  const divide3Handler = () => {
-    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
   };
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={incrementCounthandler}>+</button>
-        <button onClick={decrementCounthandler}>-</button>
-      </div>
-      <div>
-        <button onClick={incrementCounthandler2}>+</button>
-        <button onClick={decrementCounthandler2}>-</button>
-      </div>
-      <div>
-        <button onClick={resetCounthandler}>Reset</button>
-        <button onClick={doubleCountHandler}>*2</button>
-        <button onClick={divide3Handler}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(event) => setName(event.target.value)} />
     </>
   );
 };
 
 export default App;
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
+};
